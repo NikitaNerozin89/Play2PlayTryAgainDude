ea26b838dc6cf40d42fad689b7845fa5e3bdbfa3
user190393 <user190393@74-80-245-205.ic2net.net>
2021-01-04 13:37:46 -0800

    added stuff

--- Play2PlayTryAgainDude/Model.swift
+++ Play2PlayTryAgainDude/Model.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    
+    func videosFetched(_ videos:[Video])
+}
+
+
 class Model {
+    
+    var delegate:ModelDelegate?
     
     func getVideos() {
     
@@ -38,6 +46,11 @@ class Model {
             
             let response = try decoder.decode(Response.self, from: data!)
             
+            // fetch the videos
+            
+            if response.items != nil {
+                self.delegate?.videosFetched(response.items!)
+            }
             dump(response)
         }
         catch {
--- Play2PlayTryAgainDude/ViewController.swift
+++ Play2PlayTryAgainDude/ViewController.swift
@@ -7,19 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ModelDelegate {
+
+
+    @IBOutlet weak var TableView: UITableView!
     
     var model = Model()
+    var videos = [Video]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        TableView.dataSource = self
+        TableView.delegate = self
         
+        // self video
+        model.delegate = self
         model.getVideos()
-        
-      
+  
     }
-
-
+    
+    // model delegate method
+    
+    func videosFetched(_ videos: [Video]) {
+        
+        self.videos = videos
+        
+     // go again video
+        
+        TableView.reloadData()
+    }
+     
+    // mention table views method
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return videos.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        
+        cell.textLabel?.text = title
+        // Configure the cell with the data
+        
+        
+        // Return the cell
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
 }
 
