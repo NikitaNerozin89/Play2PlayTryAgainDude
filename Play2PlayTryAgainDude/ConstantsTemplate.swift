6579cff8f05b83b5f5f002cc36107a679eba7b33
user190393 <user190393@74-80-245-205.ic2net.net>
2021-01-04 10:42:53 -0800

    modified gitignore for constants template

--- Play2PlayTryAgainDude/ConstantsTemplate.swift
+++ Play2PlayTryAgainDude/ConstantsTemplate.swift
@@ -14,5 +14,4 @@ struct Constants {
 static var API_KEY = ""
 static var PLAYLIST_ID = ""
 static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key= \(Constants.API_KEY)"
-}
-*/
+}*/
