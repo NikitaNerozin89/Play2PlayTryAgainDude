e6cc5ed09bb06ef7fe5daf464f9f7e9b71dd0b8e
user190393 <user190393@74-80-245-205.ic2net.net>
2021-01-04 10:13:12 -0800

    build try

---
+++ Play2PlayTryAgainDude/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Play2PlayTryAgainDude
+//
+//  Created by user190393 on 1/4/21.
+//
+
+import Foundation
+
+
+
+struct Constants {
+
+static var API_KEY = "AIzaSyDklvyN3qfTq4JXn3AAzWRraQ2WH0yI7E0"
+static var PLAYLIST_ID = "UUlkLRHrhFmT7c_MugRxdF5g"
+static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+
